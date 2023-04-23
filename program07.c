6a3bfc5283ab680b37c6eb167cfc4ace8639446b
raziya8 <raziya.parween.info@gmail.com>
2023-04-23 08:29:00 +0530

    Write C program to check you can vote or not

---
+++ program07.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int Dividend , Divisior , Quotient , Reminder ;
+
+    printf("Enter Dividend\n");
+     scanf("%d" , & Dividend);
+     printf ("Enter Divisior\n");
+     scanf ("%d", & Divisior);
+  // computer
+  Quotient = Dividend /Divisior ;
+  // compute reminder
+Reminder = Dividend % Divisior ; 
+  printf("Quotient = %d\n", Quotient);
+  printf("Reminder = %d\n", Reminder);
+
+    return 0;
+}
