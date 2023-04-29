06ae69f52f073024fd7b7fe37bb7ea4144484017
raziya8 <raziya.parween.info@gmail.com>
2023-04-29 11:23:24 +0530

    Write C program using for loop.

---
+++ Tutorial15.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main ()
+{
+    
+    int i , j;
+    printf("Hello World\n");
+    for (i=0 ,j=0; i<5, j<9 ; j++, i++)
+
+{
+    printf("%d %d \n",i,j);
+}
+    return 0;
+
+}
