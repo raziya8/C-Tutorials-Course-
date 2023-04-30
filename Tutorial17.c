9d31be5845a27f951856d16114cf97d5a6da8f6a
raziya8 <raziya.parween.info@gmail.com>
2023-04-30 08:39:18 +0530

    Write C program of Typecasting.

--- Tutorial17.c
+++ Tutorial17.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 int main ()
 {
-    label:
-
+    
     printf("We are inside label \n");
-    goto end:
-    printf("Hello world\n");
-    return 0;
 
 }
---
+++ Tutorial18.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+ //Typecasting  syntax 
+ //(type) value;
+int main ()
+{
+    int a =3;
+    float b = (float )54/5;
+
+    printf("The value of a is %f\n", b);
+    return 0;
+
+} 
