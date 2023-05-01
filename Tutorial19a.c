c8c8430c261b67f6a2092366bc29cd05ef0de03a
raziya8 <raziya.parween.info@gmail.com>
2023-05-01 17:26:50 +0530

    Write C program to using funtion.

--- Tutorial19a.c
+++ Tutorial19a.c
@@ -10,16 +10,30 @@ void printstar(int n)
         printf("%c\n", '*');
     
 }
+
+
+
+int takenumber()
+
+    {
+        int i ;
+        printf("Enter a number  is");
+        scanf (" %d",&i);
+        return i;
+    }
+
 int main ()
 { 
     
     int a , b, c;
     a = 1;
     b = 87;
-    c = sum(a,b);
-    printstar(7);
+   // c = sum(a,b);
+    c =  takenumber();
+   // printstar(7);
  
    // printf("this sum is %d\n" ,c );
+    printf("The number entered  is  %d\n" ,c );
     return 0;
 }
  int sum(int a , int b)
